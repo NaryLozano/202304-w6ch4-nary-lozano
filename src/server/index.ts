bcf65dc7a646afabb26a5f5181a37b1388638856
nary lozano <narylozano@gmail.com>
2023-05-11 20:47:55 +0200

    Add method get to server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,19 @@
 import express from "express";
+import morgan from "morgan";
+import knownThings from "../data/knownThings.js";
 
 const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.get("/things", (req, res) => {
+  res.status(200).json(knownThings);
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: "End point not found" });
+});
 
 export default app;
